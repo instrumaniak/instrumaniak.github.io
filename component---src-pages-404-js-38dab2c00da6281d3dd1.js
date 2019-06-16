04e37c8ea755a0c53d87b1830bb30c8bcab4d86c
Raziur Rahman <instrumaniak@gmail.com>
2019-06-16 18:35:17 +0600

    Updates

---
+++ component---src-pages-404-js-38dab2c00da6281d3dd1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{153:function(e,n,t){"use strict";t.r(n);var a=t(0),l=t.n(a),o=t(36);n.default=function(){return l.a.createElement("div",{style:{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",height:"100vh"}},l.a.createElement("h1",null,"404"),l.a.createElement("p",null,"Page not found. Return to ",l.a.createElement(o.Link,{to:"/"},"Home"),"."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-38dab2c00da6281d3dd1.js.map
